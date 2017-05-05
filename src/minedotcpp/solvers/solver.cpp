c77a079b79f8ad3a480a62eea778e5d2093ec3e9
Gediminas Masaitis <gediminas.masaitis@gmail.com>
2017-05-05 21:52:02 +0300

    Fixed abs problem, changed to fabs

--- src/minedotcpp/solvers/solver.cpp
+++ src/minedotcpp/solvers/solver.cpp
@@ -20,12 +20,12 @@ point_map<solver_result>* solver::solve(const map& base_map) const
 
 	point_map<double> all_probabilities;
 	point_map<bool> all_verdicts;
-	
+
 
 	if(settings.solve_trivial)
 	{
 		solve_trivial(m, all_verdicts);
-		if (settings.stop_after_trivial_solving || should_stop_solving(all_verdicts))
+		if(settings.stop_after_trivial_solving || should_stop_solving(all_verdicts))
 		{
 			return get_final_results(all_probabilities, all_verdicts);
 		}
@@ -42,13 +42,13 @@ point_map<solver_result>* solver::solve(const map& base_map) const
 
 void solver::solve_trivial(solver_map& m, point_map<bool>& verdicts) const
 {
-	while (true)
+	while(true)
 	{
 		point_map<bool> currentRoundVerdicts;
 
-		for (auto& cell : m.cells)
+		for(auto& cell : m.cells)
 		{
-			if (cell.state != cell_state_empty)
+			if(cell.state != cell_state_empty)
 			{
 				continue;
 			}
@@ -56,14 +56,14 @@ void solver::solve_trivial(solver_map& m, point_map<bool>& verdicts) const
 			auto& filledNeighbours = neighbourEntry.by_state[cell_state_filled];
 			auto& flaggedNeighbours = neighbourEntry.by_flag[cell_flag_has_mine];
 			auto& antiflaggedNeighbours = neighbourEntry.by_flag[cell_flag_doesnt_have_mine];
-			if (filledNeighbours.size() == flaggedNeighbours.size() + antiflaggedNeighbours.size())
+			if(filledNeighbours.size() == flaggedNeighbours.size() + antiflaggedNeighbours.size())
 			{
 				continue;
 			}
 
 			auto flagging = filledNeighbours.size() == cell.hint;
 			auto clicking = flaggedNeighbours.size() == cell.hint;
-			if (clicking || flagging)
+			if(clicking || flagging)
 			{
 				for(auto& neighbour : filledNeighbours)
 				{
@@ -81,12 +81,12 @@ void solver::solve_trivial(solver_map& m, point_map<bool>& verdicts) const
 			}
 		}
 
-		if (currentRoundVerdicts.size() == 0)
+		if(currentRoundVerdicts.size() == 0)
 		{
 			return;
 		}
 		m.set_cells_by_verdicts(currentRoundVerdicts);
-		
+
 	}
 }
 
@@ -98,7 +98,7 @@ void solver::solve_separation(solver_map& m, point_map<double>& probabilities, p
 
 	find_common_border(m, common_border);
 	separate_borders(m, common_border, original_borders);
-	
+
 	for(auto& border : original_borders)
 	{
 		solve_border(border, m, true, borders);
@@ -110,7 +110,7 @@ void solver::solve_separation(solver_map& m, point_map<double>& probabilities, p
 		{
 			probabilities[probability.first] = probability.second;
 		}
-		if (should_stop_solving(verdicts))
+		if(should_stop_solving(verdicts))
 		{
 			return;
 		}
@@ -119,19 +119,19 @@ void solver::solve_separation(solver_map& m, point_map<double>& probabilities, p
 
 void solver::solve_border(border& b, solver_map& m, bool allow_partial_border_solving, std::vector<border>& borders) const
 {
-	if (settings.partial_border_solving)
+	if(settings.partial_border_solving)
 	{
-		if (allow_partial_border_solving && b.cells.size() > settings.partial_border_solve_from)
+		if(allow_partial_border_solving && b.cells.size() > settings.partial_border_solve_from)
 		{
 			//TrySolveBorderByPartialBorders(b, m);
 		}
-		if (should_stop_solving(b.verdicts))
+		if(should_stop_solving(b.verdicts))
 		{
 			borders.push_back(b);
 			return;
 		}
 
-		if (settings.border_resplitting && b.verdicts.size() > 0)
+		if(settings.border_resplitting && b.verdicts.size() > 0)
 		{
 			//var borders = TrySolveBorderByReseparating(b, m);
 			//if (borders != null)
@@ -141,7 +141,7 @@ void solver::solve_border(border& b, solver_map& m, bool allow_partial_border_so
 		}
 	}
 
-	if (b.cells.size() > settings.give_up_from)
+	if(b.cells.size() > settings.give_up_from)
 	{
 		b.solved_fully = false;
 		borders.push_back(b);
@@ -149,7 +149,7 @@ void solver::solve_border(border& b, solver_map& m, bool allow_partial_border_so
 	}
 
 	find_valid_border_cell_combinations(m, b);
-	if (b.valid_combinations.size() == 0)
+	if(b.valid_combinations.size() == 0)
 	{
 		// TODO: Must be invalid map... Handle somehow
 	}
@@ -206,7 +206,12 @@ void solver::solve_border(border& b, solver_map& m, bool allow_partial_border_so
 				break;
 			}
 		}
-		b.cells.erase(b.cells.begin() + c_index);
+		if(c_index != b.cells.size() - 1)
+		{
+			b.cells[c_index] = std::move(b.cells.back());
+		}
+		b.cells.pop_back();
+		//b.cells.erase(b.cells.begin() + c_index);
 		for(auto& valid_combination : b.valid_combinations)
 		{
 			valid_combination.erase(whole_border_result.first);
@@ -222,7 +227,7 @@ void solver::find_valid_border_cell_combinations(solver_map& map, border& border
 {
 	auto border_length = border.cells.size();
 	const int maxSize = 31;
-	if (border_length > maxSize)
+	if(border_length > maxSize)
 	{
 		// TODO: handle too big border
 		//throw new InvalidDataException($"Border with {borderLength} cells is too large, maximum {maxSize} cells allowed");
@@ -241,12 +246,12 @@ void solver::find_valid_border_cell_combinations(solver_map& map, border& border
 		CELL_INDICES_ELEMENT(cell_indices, c.pt, map) = i;
 		//border.cell_indices[c.pt] = i;
 		auto& entry = map.neighbour_cache_get(c.pt).by_state[cell_state_empty];
-		for (auto& cell : entry)
+		for(auto& cell : entry)
 		{
 			empty_pts.insert(cell.pt);
 		}
 	}
-	
+
 
 	std::vector<cell> empty_cells;
 	empty_cells.reserve(empty_pts.size());
@@ -255,28 +260,28 @@ void solver::find_valid_border_cell_combinations(solver_map& map, border& border
 		empty_cells.push_back(map.cell_get(pt));
 	}
 	//auto prediction_index = border.valid_combinations.size();
-	
+
 	for(int combo = 0; combo < totalCombinations; combo++)
 	{
-		if (map.remaining_mine_count > 0)
+		if(map.remaining_mine_count > 0)
 		{
 			auto bits_set = SWAR(combo);
-			if (bits_set > map.remaining_mine_count)
+			if(bits_set > map.remaining_mine_count)
 			{
 				continue;
 			}
-			if (allRemainingCellsInBorder && bits_set != map.remaining_mine_count)
+			if(allRemainingCellsInBorder && bits_set != map.remaining_mine_count)
 			{
 				continue;
 			}
 		}
-		
+
 		auto prediction_valid = is_prediction_valid(map, border, combo, empty_cells, cell_indices);
-		if (prediction_valid)
+		if(prediction_valid)
 		{
 			point_map<bool> predictions;
 			predictions.resize(border_length);
-			for (unsigned int j = 0; j < border_length; j++)
+			for(unsigned int j = 0; j < border_length; j++)
 			{
 				auto& pt = border.cells[j].pt;
 				auto has_mine = (combo & (1 << j)) > 0;
@@ -289,15 +294,15 @@ void solver::find_valid_border_cell_combinations(solver_map& map, border& border
 
 bool solver::is_prediction_valid(solver_map& map, border& b, unsigned int prediction, std::vector<cell>& empty_cells, CELL_INDICES_T& cell_indices) const
 {
-	for (auto& cell : empty_cells)
+	for(auto& cell : empty_cells)
 	{
 		auto neighbours_with_mine = 0;
 		auto neighbours_without_mine = 0;
 		auto& filled_neighbours = map.neighbour_cache_get(cell.pt).by_state[cell_state_filled];
-		for (auto& neighbour : filled_neighbours)
+		for(auto& neighbour : filled_neighbours)
 		{
 			auto flag = neighbour.state & cell_flags;
-			switch (flag)
+			switch(flag)
 			{
 			case cell_flag_has_mine:
 				++neighbours_with_mine;
@@ -309,15 +314,15 @@ bool solver::is_prediction_valid(solver_map& map, border& b, unsigned int predic
 				/*unsigned int i;
 				for(i = 0; i < b.cells.size(); i++)
 				{
-					if(neighbour.pt == b.cells[i].pt)
-					{
-						break;
-					}
+				if(neighbour.pt == b.cells[i].pt)
+				{
+				break;
+				}
 				}*/
 				unsigned int i = CELL_INDICES_ELEMENT(cell_indices, neighbour.pt, map);
 				//unsigned int i = b.cell_indices[neighbour.pt];
 				auto verdict = (prediction & (1 << i)) > 0;
-				if (verdict)
+				if(verdict)
 				{
 					++neighbours_with_mine;
 				}
@@ -329,9 +334,9 @@ bool solver::is_prediction_valid(solver_map& map, border& b, unsigned int predic
 			}
 		}
 
-		if (neighbours_with_mine != cell.hint)
+		if(neighbours_with_mine != cell.hint)
 			return false;
-		
+
 		// TODO: What does this do?
 		//if (filled_neighbours.size() - neighbours_without_mine < cell.hint)
 		//	return false;
@@ -349,7 +354,7 @@ int solver::SWAR(int i) const
 
 void solver::calculate_border_probabilities(border& b) const
 {
-	if (b.valid_combinations.size() == 0)
+	if(b.valid_combinations.size() == 0)
 	{
 		return;
 	}
@@ -373,17 +378,16 @@ void solver::get_verdicts_from_probabilities(point_map<double>& probabilities, p
 	for(auto& probability : probabilities)
 	{
 		bool verdict;
-		if (abs(probability.second) < 0.0000001)
+		if(fabs(probability.second) < 0.0000001)
 		{
 			verdict = false;
 		}
-		else if (abs(probability.second - 1) < 0.0000001)
+		else if(fabs(probability.second - 1) < 0.0000001)
 		{
 			verdict = true;
 		}
 		else
 		{
-			// TODO: Invalid probability, handle somehow
 			continue;
 		}
 		target_verdicts[probability.first] = verdict;
@@ -396,13 +400,13 @@ void solver::get_partial_border(solver_map& m, point_set& allowed_coordinates, p
 	std::queue<point> coord_queue;
 	coord_queue.push(target_coordinate);
 	point_set visited;
-	
-	while (coord_queue.size() > 0)
+
+	while(coord_queue.size() > 0)
 	{
 		auto& coord = coord_queue.front();
 		coord_queue.pop();
 		auto& cell = m[coord];
-		if (common_coords.find(coord) != common_coords.end())
+		if(common_coords.find(coord) != common_coords.end())
 		{
 			common_coords.erase(coord);
 			target_border.cells.push_back(cell);
@@ -433,11 +437,11 @@ void solver::separate_borders(solver_map& m, border& common_border, std::vector<
 		common_coords.insert(c.pt);
 	}
 	auto i = target_borders.size();
-	while (common_coords.size() > 0)
+	while(common_coords.size() > 0)
 	{
 		auto& initial_point = *common_coords.begin();
 		target_borders.resize(++i);
-		auto& brd = target_borders[i-1];
+		auto& brd = target_borders[i - 1];
 		get_partial_border(m, common_coords, initial_point, brd);
 		for(auto& c : brd.cells)
 		{
@@ -448,7 +452,7 @@ void solver::separate_borders(solver_map& m, border& common_border, std::vector<
 
 bool solver::is_cell_border(solver_map& m, cell& c) const
 {
-	if (c.state != cell_state_filled)
+	if(c.state != cell_state_filled)
 	{
 		return false;
 	}
@@ -473,11 +477,11 @@ bool solver::should_stop_solving(point_map<bool>& verdicts) const
 	{
 		return false;
 	}
-	if (settings.stop_on_any_verdict)
+	if(settings.stop_on_any_verdict)
 	{
 		return true;
 	}
-	if (settings.stop_on_no_mine_verdict)
+	if(settings.stop_on_no_mine_verdict)
 	{
 		for(auto& verdict : verdicts)
 		{
@@ -501,7 +505,7 @@ point_map<solver_result>* solver::get_final_results(point_map<double>& probabili
 		result.probability = probability.second;
 		result.verdict = verdict_none;
 	}
-	for (auto& verdict : verdicts)
+	for(auto& verdict : verdicts)
 	{
 		auto& result = results[verdict.first];
 		result.pt = verdict.first;
